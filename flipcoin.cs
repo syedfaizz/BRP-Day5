13031df2eb7e3ff737dd566dac3a40bcfe49432b
syedfaizz <syedfaizz45@gmail.com>
2021-10-04 10:18:31 +0530

    [faisal] Add: welcome

---
+++ flipcoin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Coin_Toss
+{
+    class Coin
+    {
+        private string sideUp;
+        Random rand = new Random();
+        public Coin()
+        {
+            sideUp = "Heads";
+        }
+        public void Toss()
+        {
+            if (rand.Next(2) == 0)
+            {
+                sideUp = "Heads";
+            }
+            else
+            {
+                sideUp = "Tails";
+            }
+        }
+        public string GetSideUp()
+        {
+            return sideUp;
+        }
+    }
+}
---
+++ poweroftwo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class program
+{
+    static bool isPowerOfTwo(int n)
+    {
+        return (int)(Math.Ceiling((Math.Log(n) / Math.Log(2))))
+              == (int)(Math.Floor(((Math.Log(n) / Math.Log(2)))));
+    }
+    public static void Main()
+    {
+        if (isPowerOfTwo(31))
+            Console.WriteLine("Yes");
+        else
+            Console.WriteLine("No");
+
+        if (isPowerOfTwo(64))
+            Console.WriteLine("Yes");
+        else
+            Console.WriteLine("No");
+    }
+}
